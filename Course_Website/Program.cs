41fd0d21eb272a6fba72e6797ba6d895af724615
tuanhuy1104pro@gmail.com <tuanhuy1104pro@gmail.com>
2024-04-12 10:36:41 +0700

    program config

--- Course_Website/Program.cs
+++ Course_Website/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
+
