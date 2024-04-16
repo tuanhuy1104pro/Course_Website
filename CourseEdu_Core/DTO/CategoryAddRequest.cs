f858b67ca306e40a0815dd0e6542e7f77f8c2df2
tuanhuy1104pro@gmail.com <tuanhuy1104pro@gmail.com>
2024-04-16 21:28:29 +0700

    Strict about data type value

--- CourseEdu_Core/DTO/CategoryAddRequest.cs
+++ CourseEdu_Core/DTO/CategoryAddRequest.cs
@@ -1,6 +1,7 @@
 ﻿using CourseEdu_Core.Domain.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace CourseEdu_Core.DTO
 {
     public class CategoryAddRequest
     {
+        [Required(ErrorMessage = "Name cannot be blank")]
         public string? CategoryName { get; set; }
         public  Category ToCategory()
         {
--- CourseEdu_Core/DTO/CourseAddRequest.cs
+++ CourseEdu_Core/DTO/CourseAddRequest.cs
@@ -1,6 +1,7 @@
 ﻿using CourseEdu_Core.Domain.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace CourseEdu_Core.DTO
 {
     public class CourseAddRequest
     {
+        [Required(ErrorMessage ="Name cannot be blank")]
         public string CourseName { get; set; }
         public string CourseDescription { get; set; }
+        [Required(ErrorMessage = "Name cannot be blank")]
+        [RegularExpression("^[0-9]+$",ErrorMessage ="Must be number")]
         public double CoursePrice { get; set; }
+        [Required(ErrorMessage = "Id cannot be blank")]
         public string CategoryId { get; set; }
 
         public Course toCourse()
